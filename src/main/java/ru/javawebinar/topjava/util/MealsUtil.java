6aed6be8f8da0fa85a52197287f97209c1eb370d
Mihail Piakhota <piakhota@bk.ru>
2023-04-04 13:42:42 +0400

    Apply 1_opt_2_HW0_cycles.patch1

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -10,6 +10,8 @@ import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static ru.javawebinar.topjava.util.TimeUtil.isBetweenHalfOpen;
+
 public class MealsUtil {
     public static void main(String[] args) {
         List<Meal> meals = Arrays.asList(
