9ef09542d9b555f57d190176a4886da4a7fc9db9
Mihail Piakhota <piakhota@bk.ru>
2023-04-06 23:01:43 +0400

    Apply 3_09_add_junit.patch

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -1,7 +1,5 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
@@ -10,11 +8,19 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static ru.javawebinar.topjava.UserTestData.*;
+
+
 @Repository
 public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
 
-    static final int USER_ID = 1;
-    static final int ADMIN_ID = 2;
+    public void init() {
+        map.clear();
+        put(user);
+        put(admin);
+        put(guest);
+        counter.getAndSet(GUEST_ID + 1);
+    }
 
     @Override
     public List<User> getAll() {
